4eddd6b92daca3af56f00ec99f673ef448d9b475
John Duran <johnduran@dts.com.do>
2021-09-10 23:51:25 -0400

    first sketch

---
+++ ContentViewAndStylesSAMPLE/ContentViewAndStylesSAMPLE/ModelViews/MenuViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ContentViewAndStylesSAMPLE.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentViewAndStylesSAMPLE.ModelViews
+{
+    class MenuViewModel
+    {
+        public MenuOption Cars { get; set; }
+        public MenuOption Parts { get; set; }
+        public MenuOption Contracts { get; set; }
+        public MenuOption Contacts { get; set; }
+        public MenuOption Promotions { get; set; }
+        public MenuOption Garages { get; set; }
+
+        public MenuViewModel()
+        {
+            Cars = new MenuOption("Carros", "car.png");
+            Parts = new MenuOption("Partes", "part.png");
+            Contracts = new MenuOption("Contratos", "contract.png");
+            Contacts = new MenuOption("Contactos", "contact.png");
+            Promotions = new MenuOption("Promociones", "promotion.png");
+            Garages = new MenuOption("Garajes", "garage.png");
+        }
+    }
+}
---
+++ ContentViewAndStylesSAMPLE/ContentViewAndStylesSAMPLE/Models/MenuOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentViewAndStylesSAMPLE.Models
+{
+    class MenuOption
+    {
+        public MenuOption(string title, string image)
+        {
+            Title = title;
+            Image = image;
+        }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
